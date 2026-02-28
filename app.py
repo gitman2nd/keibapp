16cd551d5c5aa35cc8807e7b44de3954d576ac01
gitman2nd <nikutokotoko29@gmail.com>
2026-02-28 22:23:10 +0900

    fix server for gunicorn

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from dash import Dash, html, dcc
 import plotly.express as px
 
 df = px.data.iris()
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
-                 template="plotly_white")
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", template="plotly_white")
 
 app = Dash(__name__)
+server = app.server   # ←これが必須
+
 app.layout = html.Div([
     html.H2("Dash + Plotly 動作確認"),
     dcc.Graph(figure=fig)
