5102eeadf2c47329328bb4f5cc9917680dab3449
gitman2nd <nikutokotoko29@gmail.com>
2026-02-28 21:45:46 +0900

    first deploy

---
+++ app.py
@@ -0,0 +1,15 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                 template="plotly_white")
+
+app = Dash(__name__)
+app.layout = html.Div([
+    html.H2("Dash + Plotly 動作確認"),
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
