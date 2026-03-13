b873881af24d440a6c7eebda7094b55abecc9281
gitman2nd <nikutokotoko29@gmail.com>
2026-03-14 04:05:55 +0900

    update app

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ from matplotlib.lines import Line2D
 import io, base64
 
 BASE_URL = "https://pub-3723ecad7f3943b4b6b29ffeb24bb0fb.r2.dev"
-UR_NEW_URL = f"{BASE_URL}/ur_予想用.csv"
-UR_URL = f"{BASE_URL}/ur_分析用.csv"
+UR_NEW_URL = f"{BASE_URL}/ur_yosou.parquet"
+UR_URL = f"{BASE_URL}/ur_bunseki.parquet"
 
-ur_new = pd.read_csv(UR_NEW_URL, index_col=None)
-ur = pd.read_csv(UR_URL, index_col=None)
+ur_new = pd.read_parquet(UR_NEW_URL)
+ur = pd.read_parquet(UR_URL)
 
 ur_pre = ur.merge(
     ur_new[["レースid","開催日","クラス","コース","R","芝砂","距離","馬名","騎手","馬番","枠番","距離区分"]],
